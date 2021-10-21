d777bd196faaea8b1aa7d59e14c6783355355a56
MatiasL <longomaty@gmail.com>
2021-10-21 11:28:51 -0300

    works

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express")
-const app = require("app")
 const http = require("http")
 const port = process.env.PORT || 8080
-const server = http.createServer(app)
-const router = require("./routes/index")
+//const server = http.createServer(app)
+const router = require("./routes/products")
 const router2 = require("./routes/cart")
-
-
+const app = express()
+ 
 app.use("/api", router)
-app.use("/api", router2)
+//app.use("/api", router2)
 
-server.listen(port, ()=>{
+//app.use(express.json())
+//app.use(express.urlencoded({extended: true}))
+
+
+app.listen(port, ()=>{
     console.log(`Server on port ${port} is running`)
 })
