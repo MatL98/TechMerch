bd329bf6e64377c4a92c0a405f6007bf1178bf26
MatL98 <longomaty@gmail.com>
2022-01-25 02:42:23 -0300

    fix security db

--- server/src/db.js
+++ server/src/db.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const URI =
-  "mongodb+srv://mat:fury8gb@cluster0.fpnkj.mongodb.net/ecommerce?retryWrites=true&w=majority";
+require("dotenv").config();
 
 const connectDb = () => {
-  mongoose.connect(URI, {
+  mongoose.connect(process.env.MONGODB_URI, {
     useNewUrlParser: true,
   });
   mongoose.connection.on("open", () => {
